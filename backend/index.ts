640e165d657ed034f1de5dc7b6d0db7bf2f68535
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-05-14 21:40:49 +0530

    complete streaming setup done

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,13 @@
 import express from "express"
+import {tavily} from "@tavily/core"
+import { streamText, Output } from 'ai'
+import {PROMPT_TEMPLATE, SYSTEM_PROMPT} from './prompt'
+import {z} from 'zod'
+
 
 const app=express()
 app.use(express.json())
+const client=tavily({apiKey: process.env.TAVILY_API_KEY})
 
 app.post("/poorplexity_ask", async(req, res)=>{
     //basic architecture:
@@ -21,6 +27,38 @@ app.post("/poorplexity_ask", async(req, res)=>{
     //parallely strem resources from the external api, post structuring it ofc
 
     const query=req.body.query
+
+    const webSearchResponse= await client.search(query, {
+        searchDepth: "advanced"
+    }) 
+    const webSearchResult=webSearchResponse.results
+
+    const prompt = PROMPT_TEMPLATE.
+    replace("{{WEB_SEARCH_RESULTS}}", JSON.stringify(webSearchResult))
+    .replace("{{USER_QUERY}}", query)
+    //hittin llm
+    const result = streamText({
+        model: 'openai/gpt-5.5',
+        prompt: prompt,
+        system: SYSTEM_PROMPT,
+    })
+
+    //headers to let it know its a stream
+    res.header('Cache-control', 'no-cache')
+    res.header('Content-Type', 'text/event-stream')
+
+    for await(const textPart of result.textStream){
+        res.write(textPart)
+        //todo pass it to frontend
+    }
+
+    res.write("\n <SOURCES> \n")
+     
+    res.write(JSON.stringify(webSearchResult))
+    
+    res.write("\n <SOURCES> \n")
+
+    res.end()
 })
 
-app.listen(3000)
+app.listen(3000)  
