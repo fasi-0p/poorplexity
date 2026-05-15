a22fd773551b1a934ab045ff1d8f645c7771fd0d
Fasi Owaiz Ahmed <fasiowaizahmed1234@gmail.com>
2026-05-16 00:46:31 +0530

    db set up done

--- backend/generated/internal/class.ts
+++ backend/generated/internal/class.ts
@@ -48,7 +48,7 @@ const config: runtime.GetPrismaClientConfig = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": true,
+  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
